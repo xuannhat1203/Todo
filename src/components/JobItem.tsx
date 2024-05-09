3e1996eb487f23a1213ad6beb4ae30d260abc02d
xuannhat <xuannhatvn211@gmail.com>
2024-05-09 22:21:06 +0700

    todolist

--- src/components/JobItem.tsx
+++ src/components/JobItem.tsx
@@ -1,27 +1,34 @@
-type Job = {
-    id:string,
-    name:string,
-    status:boolean,
-}
-type FunctionType = {
-    job:Job,
-    handleChangeStatus: (id:string) => any,
-}
-export default function JobItem({job,handleChangeStatus}:FunctionType) {
-    const handleChecked = (id:string) => {
-        // truyền id sang component cha
-        handleChangeStatus(id);
-    }
 
+type Job = {
+  id: string;
+  name: string;
+  status: boolean;
+};
+
+type FunctionType = {
+  job: Job;
+  handleChangeStatus: (id: string) => any;
+};
+
+export default function JobItem({ job, handleChangeStatus }: FunctionType) {
+  const handleChecked = (id: string) => {
+    // truyền
+    handleChangeStatus(id);
+  };
   return (
     <>
       <li key={job.id}>
-        <input onChange={()=>handleChecked(job.id)} type="checkbox" id="task1" checked = {job.status} />
+        <input
+          onChange={() => handleChecked(job.id)}
+          type="checkbox"
+          id="task1"
+          checked={job.status}
+        />
         <label htmlFor="task1">
-            {job.status?<s>{job.name}</s>:<p>{job.name}</p>}
+          {job.status ? <s>{job.name}</s> : <p>{job.name}</p>}
         </label>
         <button>Delete</button>
       </li>
     </>
-  )
+  );
 }
